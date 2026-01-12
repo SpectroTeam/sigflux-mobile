777e4a216a267347027a1ab507adbda8d69b0a89
ericksjp <ericksjp703.gmail.com>
2026-01-12 17:17:40 -0300

    feat(fontes): adiciona Josefin Sans Bold ao hook useFonts

--- src/hooks/useFonts.ts
+++ src/hooks/useFonts.ts
@@ -1,10 +1,11 @@
 import { useFonts as useExpoFonts } from "expo-font";
-import { JosefinSans_400Regular } from "@expo-google-fonts/josefin-sans";
+import { JosefinSans_400Regular, JosefinSans_700Bold } from "@expo-google-fonts/josefin-sans";
 import { TiltWarp_400Regular } from "@expo-google-fonts/tilt-warp";
 
 export function useFonts() {
     const [fontsLoaded] = useExpoFonts({
         "Josefin Sans": JosefinSans_400Regular,
+        "Josefin Sans-Bold": JosefinSans_700Bold,
         "Tilt Warp": TiltWarp_400Regular,
     });
 
