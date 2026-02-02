e0b0a9a2a1ffd2e2be044699001110982fdfebf7
Erick Ribeiro <ericksjp703@gmail.com>
2026-02-01 22:19:39 -0300

    refactor(component): ajusta elevation do Componente MenuOption

--- src/components/Home/MenuOption.tsx
+++ src/components/Home/MenuOption.tsx
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         ...SHADOWS.md,
+        elevation: 0.
     },
     iconContainer: {
         justifyContent: "center",
